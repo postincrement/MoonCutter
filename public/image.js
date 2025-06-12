74fd275072b19aa609087dbadfe118964aa3a3e4
Craig Southeren <craig@postincrement.com>
2025-06-13 00:12:15 +1000

    Update the MPL 2.0 license

--- public/image.js
+++ public/image.js
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 let g_imageSettings = {
   m_rotateAngle: 0,
   m_imageOffsetX: 0,
